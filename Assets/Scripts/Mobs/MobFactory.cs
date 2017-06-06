f04e2a7e4c01150678748044750393135334379d
Sam Kern <samkern108@gmail.com>
2017-06-05 21:31:19 -0700

    Dumb reformatting.

--- Assets/Scripts/Mobs/MobFactory.cs
+++ Assets/Scripts/Mobs/MobFactory.cs
@@ -6,64 +6,66 @@ using System.Text.RegularExpressions;
 
 public class MobFactory : MonoBehaviour 
 {
-		private float health = 0;
-		private float damage = 0;
-		private float speed = 0;
-		private float armor = 0;
-		private float plague = 0;
-		private float number = 0;
+	private float health = 0;
+	private float damage = 0;
+	private float speed = 0;
+	private float armor = 0;
+	private float plague = 0;
+	private float number = 0;
 
-		public GameObject zombie;
-		public Transform mobManager;
-		public Image preview;
+	public GameObject zombie;
+	public Transform mobManager;
+	public Image preview;
 	
-		public void SetHealth(float h)
-		{
-				health = h;
-				//TODO update preview so that the healthier zombies get larger and meatier and the unhealthier zombies lose flesh until they are skeletons
-		}
-		
-		public void SetDamage(float d)
-		{
-				damage = d;
-				//TODO update preview so that the high damage zombies get better and better weapons and the lower damage zombies just get nothing or bad weapons
-		}
-	
-		public void SetSpeed(float s)
-		{
-				speed = s;
-				//TODO update preview so that the faster zombies become zombie dogs and the slower zombies all have lost limbs
-		}
-	
-		public void SetArmor(float a)
-		{
-				armor = a;
-				//TODO update preview so that the armored zombies have suits of armor and the unarmored zombies have none
-		}
+	public void SetHealth(float h)
+	{
+			health = h;
+			//TODO update preview so that the healthier zombies get larger and meatier and the unhealthier zombies lose flesh until they are skeletons
+	}
 
-		public void SetPlague(float p)
-		{
-				plague = p;
-				//TODO update preview so that the plague zombies emit disease clouds and the unplagued zombies emit none
-		}
+	public void SetDamage(float d)
+	{
+			damage = d;
+			//TODO update preview so that the high damage zombies get better and better weapons and the lower damage zombies just get nothing or bad weapons
+	}
+	
+	public void SetSpeed(float s)
+	{
+			speed = s;
+			//TODO update preview so that the faster zombies become zombie dogs and the slower zombies all have lost limbs
+	}
+	
+	public void SetArmor(float a)
+	{
+			armor = a;
+			//TODO update preview so that the armored zombies have suits of armor and the unarmored zombies have none
+	}
 
-		public void SetNumber(float n)
-		{
-				number = n;		
-		}
+	public void SetPlague(float p)
+	{
+		plague = p;
+		//TODO update preview so that the plague zombies emit disease clouds and the unplagued zombies emit none
+	}
+
+	public void SetNumber(float n)
+	{
+		number = n;		
+	}
+
+	//TODO add 6 slots of predefined mobs for quick spawning.
 				
-		public void Spawn()
-		{
-				GameObject mobGroup = new GameObject ();
-				mobGroup.name = "Mob Group";
-				mobGroup.AddComponent <PlaceZombie>();
-				mobGroup.transform.parent = mobManager;
+	public void Spawn()
+	{
+		GameObject mobGroup = new GameObject ();
+		mobGroup.name = "Mob Group";
+		mobGroup.AddComponent <PlaceZombie>();
+		mobGroup.transform.parent = mobManager;
 
-				for(int i = 0; i < number; i++) {
-						GameObject newMob = GameObject.Instantiate (zombie) as GameObject;
-						newMob.transform.position += new Vector3 (Random.value * 2, Random.value * 2);
-						newMob.GetComponent<Zombie>().SetMyStats(health, armor, plague, damage, speed);
-						newMob.transform.parent = mobGroup.transform;
-				}
+		for(int i = 0; i < number; i++) {
+			GameObject newMob = GameObject.Instantiate (zombie) as GameObject;
+			newMob.transform.position += new Vector3 (Random.value * 2, Random.value * 2);
+			newMob.GetComponent<Zombie>().SetMyStats(health, armor, plague, damage, speed);
+			newMob.transform.parent = mobGroup.transform;
 		}
+	}
 }
--- Assets/Scripts/Mobs/PlaceZombie.cs
+++ Assets/Scripts/Mobs/PlaceZombie.cs
@@ -3,32 +3,32 @@ using System.Collections;
 
 public class PlaceZombie : MonoBehaviour {
 	
-		Vector3 mousePosition;
-		bool mouseDown = false;
-		bool placeUnit = false;
+	Vector3 mousePosition;
+	bool mouseDown = false;
+	bool placeUnit = false;
 
-		void Update () 
-		{
-				mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-				mousePosition.z = 0;
-				mouseDown = Input.GetMouseButtonDown (0);
+	void Update () 
+	{
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePosition.z = 0;
+		mouseDown = Input.GetMouseButtonDown (0);
 
-				if (!placeUnit) {
-						transform.position = mousePosition;
-						if (mouseDown) {
-								placeUnit = true;
-						}
-				} else {
-						if (mouseDown) {
-								PlaceUnit (mousePosition);
-						}
-				}
+		if (!placeUnit) {
+			transform.position = mousePosition;
+			if (mouseDown) {
+				placeUnit = true;
+			}
+		} else {
+			if (mouseDown) {
+				PlaceUnit (mousePosition);
+			}
 		}
+	}
 
-		private void PlaceUnit(Vector3 target)
-		{
-				this.gameObject.AddComponent <MobMovement>();
-				this.gameObject.GetComponent <MobMovement>().SetTarget(target, 1.0f);
-				Destroy (this);
-		}
+	private void PlaceUnit(Vector3 target)
+	{
+		this.gameObject.AddComponent <MobMovement>();
+		this.gameObject.GetComponent <MobMovement>().SetTarget(target, 1.0f);
+		Destroy (this);
+	}
 }
