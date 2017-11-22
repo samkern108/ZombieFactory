a60dc6d65a62e1d45951e579dd393faab8468feb
Sam Kern <samkern108@gmail.com>
2017-11-21 22:37:51 -0800

    Made sorting layers more awesome :D

--- Assets/SortingLayer.cs
+++ Assets/SortingLayer.cs
@@ -6,16 +6,36 @@ public class SortingLayer : MonoBehaviour {
 
 	private SpriteRenderer spriteRenderer;
 
-	public int sortingOrder;
+	public bool stationary = false;
+
+	private float heightOffset;
+
+	// I played around with offset percentages to find the perfect number 
+	// at which objects disappear behind each other.
+	// This might have to change with units of different sizes/with different overlay rules,
+	// but for now, this magic number looks fine.
+	private const float kMagicHeightMultiplier = .11f;
 
 	void Start() {
 		spriteRenderer = GetComponent <SpriteRenderer>();
-	}
 
-	void Update () {
-		int pos = Mathf.RoundToInt(transform.position.y * 100);
+		heightOffset = transform.localScale.y * kMagicHeightMultiplier;
+
+		// If the object never moves, update the sorting layer once and then deactivate this.
+		if (stationary) {
+			UpdateSortingLayer();
+			this.enabled = false;
+		}
+	}
+		
+	private void UpdateSortingLayer()
+	{
+		int pos = Mathf.RoundToInt((transform.position.y - heightOffset) * 100);
 		pos /= 3;
-		spriteRenderer.sortingOrder = (pos * -1);// + OrderOffset;
-		sortingOrder = spriteRenderer.sortingOrder;
+		spriteRenderer.sortingOrder = (pos * -1);
+	}
+		
+	void Update () {
+		UpdateSortingLayer ();
 	}
 }
